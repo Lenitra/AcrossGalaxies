200946fe5e78f6da1e8c40390a83ded0c1548593
Lenitra <lenitramc@gmail.com>
2021-10-11 12:36:15 +0200

    ???

--- ex.py
+++
@@ -1,8 +0,0 @@
-from captcha.image import _Captcha, ImageCaptcha
-
-image = ImageCaptcha(width=280, height=90)
-
-capt_text = "Something"
-data = image.generate(capt_text)
-
-image.write(capt_text, "static/imgs/CAPTCHA.png")
